e77edcd2a97b6c570844555f32ba188042cbcedd
Michael Redig <mredig@gmail.com>
2020-07-03 15:34:41 -0500

    (refactor) more connection creation breakdown

--- Sources/App/Modules/Symbols/Controllers/SymbolConnectionController.swift
+++ Sources/App/Modules/Symbols/Controllers/SymbolConnectionController.swift
@@ -3,11 +3,19 @@ import Fluent
 
 struct SymbolConnectionController {
 
-	func getConnectionBetween(symbolID: UUID, andTagID tagID: UUID, req: Request) -> EventLoopFuture<SymbolTagConnection?> {
-		return SymbolTagConnection.query(on: req.db)
+	func getConnectionBetween(symbolID: UUID, andTagID tagID: UUID, request: Request) -> EventLoopFuture<SymbolTagConnection?> {
+		return SymbolTagConnection.query(on: request.db)
 			.filter(\.$symbol.$id == symbolID)
 			.filter(\.$tag.$id == tagID)
 			.first()
+	}
+
+	func getConnectionBetween(symbol: EventLoopFuture<SymbolModel>, andTag tag: EventLoopFuture<SymbolTag>, request: Request) -> EventLoopFuture<SymbolTagConnection?> {
+		return symbol.flatMap { symbolModel in
+			return tag.flatMap { tagModel in
+				return getConnectionBetween(symbolID: symbolModel.id!, andTagID: tagModel.id!, request: request)
+			}
+		}
 	}
 
 	/// Retrieves existing tag from database if it exists, creating it if it doesnt.
@@ -41,46 +49,40 @@ struct SymbolConnectionController {
 			.first()
 			.unwrap(or: Abort(.badRequest))
 
-		return symbol.flatMap { symbol -> EventLoopFuture<SymbolModel.GetContent> in
-			let tag = getCreateTag(named: connectReference.tagValue, request: req)
+		let tag = getCreateTag(named: connectReference.tagValue, request: req)
 
-			let connection = tag.flatMap { tag -> EventLoopFuture<SymbolTagConnection?> in
-				return getConnectionBetween(
-					symbolID: connectReference.symbolID,
-					andTagID: tag.id!,
-					req: req)
-			}
+		let connection = getConnectionBetween(symbol: symbol, andTag: tag, request: req)
 			.flatMap { optConnection -> EventLoopFuture<SymbolTagConnection> in
-				// this is explicitly to create a connection. if one already exists, abort
-				if optConnection != nil {
+				// this is explicitly to CREATE a connection. if one already exists, abort
+				guard optConnection == nil else {
 					return req.eventLoop.future(error: Abort(.conflict))
-				} else {
-					//create connection
-					return tag.flatMap { tag -> EventLoopFuture<SymbolTagConnection> in
-						let connection = SymbolTagConnection(tagID: tag.id!,
-															 symbolID: connectReference.symbolID,
-															 createdBy: user.id!)
-						return connection.create(on: req.db)
-							.flatMap { req.eventLoop.future(connection) }
-					}
+				}
+				//create connection
+				return tag.flatMap { tag -> EventLoopFuture<SymbolTagConnection> in
+					let connection = SymbolTagConnection(tagID: tag.id!,
+														 symbolID: connectReference.symbolID,
+														 createdBy: user.id!)
+					return connection.create(on: req.db)
+						.flatMap { req.eventLoop.future(connection) }
 				}
 			}
 
+		return symbol.flatMap { symbolModel -> EventLoopFuture<SymbolModel.GetContent> in
 			return connection.flatMap { _ -> EventLoopFuture<SymbolModel.GetContent> in
-				let loads = symbol.$connections.load(on: req.db)
+				let loads = symbolModel.$connections.load(on: req.db)
 					.flatMap { _ -> EventLoopFuture<Void> in
-						let tagLoads = symbol.connections.map { connection in
+						let tagLoads = symbolModel.connections.map { connection in
 							connection.$tag.load(on: req.db)
 						}
 						return req.eventLoop.flatten(tagLoads)
 					}
 
 				return loads.flatMap { _ in
-					let tags = symbol.$connections.value?
+					let tags = symbolModel.$connections.value?
 						.map(\.$tag.value?.listContent)
 						.compactMap({ $0 })
 
-					var getContent = symbol.getContent
+					var getContent = symbolModel.getContent
 					getContent.tags = tags
 					return req.eventLoop.future(getContent)
 				}

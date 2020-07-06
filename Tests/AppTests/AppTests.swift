79d970e28346f4d918b2f49f68a2c8cbb687e493
Michael Redig <mredig@gmail.com>
2020-07-06 04:05:16 -0500

    (nit) more heroku appeasement

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -3,7 +3,13 @@ import Fluent
 import XCTVapor
 
 final class AppTests: XCTestCase {
+    func testStub() throws {
+        XCTAssert(true)
+    }
 
+    static let allTests = [
+        ("testStub", testStub),
+    ]
 }
 
 extension DatabaseID {
